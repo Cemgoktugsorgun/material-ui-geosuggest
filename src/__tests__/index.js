1401c2752722a0bca3e2ac8e6f4e739f2d153aaf
cgoktug <cgoktug@eriktronik.com>
2017-01-20 10:58:23 +0300

    first commit

---
+++ src/__tests__/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {shallow, mount, render} from 'enzyme';
+import {expect} from 'chai';
+import sinon from 'sinon';
+
+import TextField from 'material-ui/TextField';
+import MuiGeoSuggest from '../index';
+
+describe('Shallow rendering', () => {
+    it('should render a textfield', () => {
+        const wrapper = shallow(<MuiGeoSuggest />);
+        expect(wrapper.find(<TextField />));
+    });
+});
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TextField from 'material-ui/TextField';
+
+class MuiGeoSuggest extends React.Component {
+    render() {
+        return (
+          <TextField />  
+        );
+    }
+};
+
+export default MuiGeoSuggest;
